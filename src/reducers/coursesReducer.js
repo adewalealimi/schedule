6f33a3f982a24428a0fee841b588d724aa44bf1a
adewalealimi <alimiadewale68@gmail.com>
2021-02-28 03:16:31 -0800

    created toggleCourse action creator and reducer and connected libraryCourse component to redux to call that within action.js

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -1,4 +1,8 @@
-import { FETCH_COURSES } from '../actions/types';
+import { 
+    FETCH_COURSES,
+    TOGGLE_ENROLLED
+} from '../actions/types';
+
 export default function(state = [], action) {
     switch (action.type) {
         case FETCH_COURSES:
@@ -6,7 +10,10 @@ export default function(state = [], action) {
                 ...state,
                 ...action.payload
             ]
-        default:
-            return state;
+            case TOGGLE_ENROLLED:
+                console.log(action.payload)
+                return state
+            default:
+                return state;
+        }
     }
-}
